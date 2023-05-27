3539088ac988dcf2d9ce4dbdafb01a762296b89b
Widad Toumi <wddtoumi@gmail.com>
2023-05-27 03:47:13 +0100

    deploy backend to infinityfreeapp

--- client/src/App.vue
+++ client/src/App.vue
@@ -1516,7 +1516,7 @@ export default {
     async submitForm() {
       try {
         const response = await axios.post(
-          "https://anmeldung.netlify.app/generate-pdf",
+          "https://anmeldung.infinityfreeapp.com/generate-pdf",
           {
             formData: this.formData,
           },
