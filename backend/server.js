6bd541910d8783a6ac3f2717bcf4e2f58d5f2e21
Widad Toumi <wddtoumi@gmail.com>
2023-05-28 03:29:41 +0100

    modify the app listner

--- backend/server.js
+++ backend/server.js
@@ -896,6 +896,6 @@ app.post("/generate-pdf", async (req, res) => {
   res.send(pdf);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(process.env.PORT || port, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${process.env.PORT || port}`);
 });
